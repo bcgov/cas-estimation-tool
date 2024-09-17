969f71984b77ed824a1e0d654bbde28b71d42666
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-09-17 11:18:51 -0700

    chore: allowing db connection from env vars

--- estimation/estimation/settings.py
+++ estimation/estimation/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,10 +77,11 @@ WSGI_APPLICATION = "estimation.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "estimation",  # database name
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.environ.get("PGDATBASE", "estimation"),  # database name
+        "USER": os.environ.get("PGUSER", "postgres"),
+        "PASSWORD": os.environ.get("PGPASSWORD"),
+        "HOST": os.environ.get("PGHOST", "localhost"),
+        "PORT": os.environ.get("PGPORT", "5432"),
         "OPTIONS": {
             "pool": True,
         },
