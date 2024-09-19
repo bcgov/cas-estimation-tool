aaff0b3d6014629b2b3dead6d5eedd48a72a3f7c
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2024-09-19 11:23:45 -0700

    chore: gh action

---
+++ helm/deploy_db.sh
@@ -0,0 +1,12 @@
+
+set -euo pipefail
+
+echo "Installing database chart: cas-postgres/cas-postgres-cluster..."
+
+helm repo add cas-postgres https://bcgov.github.io/cas-postgres/
+helm repo update
+
+helm upgrade --install --atomic --timeout 1800s \
+  --namespace "cf52af-test" \
+  --values ./database/values.yaml \
+  cas-estimation-db cas-postgres/cas-postgres-cluster --version 1.1.1
